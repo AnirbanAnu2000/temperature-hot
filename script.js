60385b099a2d76b3202f05862149f3e9e9537c6f
AnirbanAnu2000 <anirbananu2000@gmail.com>
2021-08-31 12:04:05 +0600

    remove momments from script file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getWeather() {
     const inputField = document.getElementById('inputField');
     const cityName = inputField.value;
     const apiKey = '42b3774f1eef491b1c4f65e00376da9b';
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
     inputField.value = '';
     fetch(url)
         .then(res => res.json())
