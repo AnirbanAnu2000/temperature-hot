cbc16026803311a0ac45301afe8f0442cf7b284c
AnirbanAnu2000 <anirbananu2000@gmail.com>
2021-08-31 14:28:49 +0600

    some edit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function getWeather() {
     const inputField = document.getElementById('inputField');
     const cityName = inputField.value;
-    const apiKey = '42b3774f1eef491b1c4f65e00376da9b';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-    inputField.value = '';
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayWeatherForcust(data));
-    console.log(url);
+    if (cityName == '') {
+        const city = document.getElementById('city');
+        city.innerText = 'Please enter your city name.';
+    } else {
+        const apiKey = '42b3774f1eef491b1c4f65e00376da9b';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+        inputField.value = '';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayWeatherForcust(data));
+        console.log(url);
+    }
 }
 
 function displayWeatherForcust(temp) {
@@ -15,7 +20,9 @@ function displayWeatherForcust(temp) {
     city.style.fontSize = '50px';
     city.innerText = temp.name;
     const dayTemp = document.getElementById('temp');
-    dayTemp.innerText = Math.round(temp.main.temp - 273);
+    // dayTemp.innerText = Math.round(temp.main.temp - 273);
+    const temInDeg = temp.main.temp - 273;
+    dayTemp.innerText = temInDeg.toFixed(2);
     const desc = document.getElementById('desc');
     desc.innerText = temp.weather[0].main;
 }
